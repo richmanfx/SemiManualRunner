884ae160cce0f93711494d1594d14f0177d61052
Aleksandr Jashhuk <richmanfx@yandex.ru>
2016-08-15 22:31:07 +0300

    Получена ссылка из XML файла тест-скрипта с помощью XPath.

--- src/main/java/ru/r5am/SemiManualRunner.java
+++ src/main/java/ru/r5am/SemiManualRunner.java
@@ -35,6 +35,8 @@ public class SemiManualRunner {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
         dateFormat.setTimeZone(tz);
 
+        System.out.println("Начало работы: " + dateFormat.format(new Date()));
+
         // Обрабатываем аргументы командной строки
         CommandLineArguments arguments = getCommandLineArguments(args);
 
@@ -43,61 +45,110 @@ public class SemiManualRunner {
         // System.out.println("Имя файла тестового скрипта: " + testScriptNameFile);
 
         // Обработка XML-конфигов
-        if(arguments.production) {        // Работа на продакшн сервере
+        String pathToConfigs;
+        if(arguments.production) {
             System.out.println("Работаем на продакшн сервере.");
-            // Считываем конфиги
-            String pathToProdConfigs = "src\\main\\resources\\prod_configs\\";
+            pathToConfigs = "src\\main\\resources\\prod_configs\\";
         } else {
             System.out.println("Работаем на тестовом сервере");
-            // Считываем конфиги
-            String pathToTestConfigs = "src\\main\\resources\\test_configs\\";
-            // Загрузить конфиг в объект Document
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            factory.setNamespaceAware(true);        // Никогда не забывай об этом!
-            DocumentBuilder builder = factory.newDocumentBuilder();
-//            System.out.println("Файл: " + pathToTestConfigs + ONLINE_SETTINGS_CONFIG_FILE);
-            Document doc = builder.parse(pathToTestConfigs + ONLINE_SETTINGS_CONFIG_FILE);
-            // Создать XPathFactory
-            XPathFactory myXPathFactory = XPathFactory.newInstance();
-            // Используется эта фабрика для создания объекта XPath
-            XPath xpath = myXPathFactory.newXPath();
-            // XPath компилирует XPath-выражение
-            XPathExpression expr = xpath.compile("//*");
-            // Выполнить запрос XPath для получения результата
-            Object result = expr.evaluate(doc, XPathConstants.NODESET);
-            // сохранить результат в DOM NodeList
-            NodeList nodes = (NodeList) result;
+            pathToConfigs = "src\\main\\resources\\test_configs\\";
+        }
+        // Считываем конфиги
+        NodeList nodesOnlineSettings = getXMLConfigNodeList(ONLINE_SETTINGS_CONFIG_FILE, pathToConfigs);
+        NodeList nodesTestRuntime = getXMLConfigNodeList(TEST_RUNTIME_CONFIG_FILE, pathToConfigs);
+        NodeList nodesTestPropertiesOnline = getXMLConfigNodeList(TESTPROPERTIES_ONLINE_FILE, pathToConfigs);
+        NodeList nodesUTestNumberOnline = getXMLConfigNodeList(testScriptNameFile, pathToConfigs);
 
-            // Выводим результат - значения параметров
-            for (int i = 0; i < nodes.getLength() ; i++) {
-                System.out.println(nodes.item(i).getLocalName() + ": " + nodes.item(i).getTextContent() );
+/*        // Выводим проверочный результат - значения параметров из XML файла
+        for (int i = 0; i < nodesUTestNumberOnline.getLength() ; i++) {
+            System.out.println(nodesUTestNumberOnline.item(i).getLocalName() + ": " +
+                               nodesUTestNumberOnline.item(i).getTextContent() );
+        }
+*/
+
+        // Запускаем требуемый браузер
+        WebDriver driver = null;
+        try {
+            if (arguments.firefox) {
+                System.out.println("Используется Firefox");
+//                driver = startFirefox();
+            } else {
+                System.out.println("Используется Chrome (по-умолчанию)");
+                driver = startChrome(PATH_TO_CHROMEDRIVER_EXE);
             }
-
+        } catch (Exception e) {
+            System.out.println("Ошибка запуска браузера: " + e.toString());
         }
 
-        System.out.println("Начало работы: " + dateFormat.format(new Date()));
 
-        System.setProperty("webdriver.chrome.driver", PATH_TO_CHROMEDRIVER_EXE);   // Выставить путь к ChromeDriver
+        // Открыть страницу
+        String siteName = getValueFromXMLConfig(testScriptNameFile, pathToConfigs,
+                                "ONLINE_DATA_DOCUMENT/PARAMETERS/NAME[text()='serviceDirectLink']/following-sibling::*"
+                                               ).split("//")[1].split("/")[0];
+
+        System.out.println("На сайт: " + siteName);
+
+        driver.get("http://" + siteName);
+
+
+
+
+        if(driver != null)  // Если браузер был успешно запущен
+            driver.quit();  // Покинуть driver, закрыть связанные с ним окна
+
+        System.out.println("Окончание работы: " + dateFormat.format(new Date()));
+    }
+
+    private static WebDriver startChrome(String pathToChromedriverExe) {
+        System.setProperty("webdriver.chrome.driver", pathToChromedriverExe);   // Выставить путь к ChromeDriver
 
         // Опции командной строки браузера
         ChromeOptions option = new ChromeOptions();
         option.addArguments("--window-size=1024,768");      // Но больше текущего разрешения экрана не делает окно
-
-        WebDriver driver = new ChromeDriver(option);      // Драйвер Chrome
-        // driver.manage().window().maximize();            // Максимизировать размер окна браузера
-
-
-
-        // Посетить страницу
-      //  driver.get("http://google.com");
-
-
-
-
-
-        driver.quit();  // Покинуть driver, закрыть связанные с ним окна
-        System.out.println("Окончание работы: " + dateFormat.format(new Date()));
+        WebDriver driver = new ChromeDriver(option);
+        driver.manage().window().maximize();            // Максимизировать размер окна браузера
+        return driver;
     }
+
+    private static NodeList getXMLConfigNodeList(String ONLINE_SETTINGS_CONFIG_FILE, String pathToConfigs)
+                        throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+        // Загрузить конфиг в объект Document
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);        // Никогда не забывай об этом!
+        DocumentBuilder builder = factory.newDocumentBuilder();
+//            System.out.println("Файл: " + pathToTestConfigs + ONLINE_SETTINGS_CONFIG_FILE);
+        Document doc = builder.parse(pathToConfigs + ONLINE_SETTINGS_CONFIG_FILE);
+        // Создать XPathFactory
+        XPathFactory myXPathFactory = XPathFactory.newInstance();
+        // Используется эта фабрика для создания объекта XPath
+        XPath xpath = myXPathFactory.newXPath();
+        // XPath компилирует XPath-выражение
+        XPathExpression expr = xpath.compile("//*");
+        // Выполнить запрос XPath для получения результата
+        Object result = expr.evaluate(doc, XPathConstants.NODESET);
+        // сохранить результат в DOM NodeList
+        return (NodeList) result;
+    }
+
+    private static String getValueFromXMLConfig(String ONLINE_SETTINGS_CONFIG_FILE, String pathToConfigs, String xPath)
+                    throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+        // Загрузить конфиг в объект Document
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);        // Никогда не забывай об этом!
+        DocumentBuilder builder = factory.newDocumentBuilder();
+//            System.out.println("Файл: " + pathToTestConfigs + ONLINE_SETTINGS_CONFIG_FILE);
+        Document doc = builder.parse(pathToConfigs + ONLINE_SETTINGS_CONFIG_FILE);
+        // Создать XPathFactory
+        XPathFactory myXPathFactory = XPathFactory.newInstance();
+        // Используется эта фабрика для создания объекта XPath
+        XPath xpath = myXPathFactory.newXPath();
+        // XPath компилирует XPath-выражение
+        XPathExpression expr = xpath.compile(xPath);
+        // Выполнить запрос XPath для получения результата
+        Object result = expr.evaluate(doc, XPathConstants.STRING);
+        return result.toString();
+    }
+
 
     // Обработка аргументов командной строки (args4j)
     private static CommandLineArguments getCommandLineArguments(String[] args) {
@@ -115,10 +166,7 @@ public class SemiManualRunner {
         if(arguments.testNumber != null)
             System.out.println("Номер теста: " + arguments.testNumber);
 
-        if(arguments.firefox)
-            System.out.println("Используется Firefox");
-        else
-            System.out.println("Используется Chrome (по-умолчанию)");
+
 
 //        System.out.println("Другие аргументы:");
 //        for( String s : arguments.extraArgs )
